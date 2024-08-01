8a07325b6c730c8151da253328fe7c5bba0227a1
ianlee <ethian9227@gmail.com>
2024-08-01 23:02:12 +0900

    회원가입 스크린 구현

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -20,4 +20,26 @@ function validateLogin(values: UserInformation) {
   return errors;
 }
 
-export {validateLogin};
+function validateSignup(values: UserInformation & {passwordConfirm: string}) {
+  const errors = {
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  };
+
+  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
+    errors.email = '올바른 이메일 형식이 아닙니다.';
+  }
+
+  if (!(values.password.length >= 8 && values.password.length < 20)) {
+    errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
+  }
+
+  if (values.password !== values.passwordConfirm) {
+    errors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+  }
+
+  return errors;
+}
+
+export {validateLogin, validateSignup};

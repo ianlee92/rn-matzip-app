b235442e8ae13a530ee169beb275a1d7d4a2d88d
ianlee <ethian9227@gmail.com>
2024-08-01 23:04:32 +0900

    validate 리팩토링

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -3,7 +3,7 @@ type UserInformation = {
   password: string;
 };
 
-function validateLogin(values: UserInformation) {
+function validateUser(values: UserInformation) {
   const errors = {
     email: '',
     password: '',
@@ -20,26 +20,19 @@ function validateLogin(values: UserInformation) {
   return errors;
 }
 
+function validateLogin(values: UserInformation) {
+  return validateUser(values);
+}
+
 function validateSignup(values: UserInformation & {passwordConfirm: string}) {
-  const errors = {
-    email: '',
-    password: '',
-    passwordConfirm: '',
-  };
-
-  if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(values.email)) {
-    errors.email = '올바른 이메일 형식이 아닙니다.';
-  }
-
-  if (!(values.password.length >= 8 && values.password.length < 20)) {
-    errors.password = '비밀번호는 8~20자 사이로 입력해주세요.';
-  }
+  const errors = validateUser(values);
+  const signupErrors = {...errors, passwordConfirm: ''};
 
   if (values.password !== values.passwordConfirm) {
-    errors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
+    signupErrors.passwordConfirm = '비밀번호가 일치하지 않습니다.';
   }
 
-  return errors;
+  return signupErrors;
 }
 
 export {validateLogin, validateSignup};

f80fcbbfbf68d4e3bd60734196807564c03e1857
ianlee <ethian9227@gmail.com>
2024-08-15 13:11:06 +0900

    mac mini

--- front/src/constants/navigations.ts
+++
@@ -1,7 +0,0 @@
-const authNavigations = {
-  AUTH_HOME: 'AuthHome',
-  LOGIN: 'Login',
-  SIGNUP: 'Signup',
-} as const;
-
-export {authNavigations};
--- front/src/constants/numbers.ts
+++
@@ -1,6 +0,0 @@
-const numbers = {
-  // 30분 - 3분 = 27분
-  ACCESS_TOKEN_REFRESH_TIME: 1000 * 60 * 30 - 1000 * 60 * 3,
-} as const;
-
-export {numbers};
